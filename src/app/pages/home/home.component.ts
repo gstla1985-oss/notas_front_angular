13b6550cd0e644ccc87883a9a2a1db8f86f69f12
djarae <djaraestivill@gmail.com>
2026-07-21 20:05:47 -0400

    feat avance home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -124,7 +124,7 @@ import { CommonModule } from '@angular/common';
                </div>
              } @else {
                <button class="add-cat-btn-floating glass" (click)="isCreatingCategory.set(true)" title="Nueva Categoría">
-                 +
+                 <svg width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
                </button>
              }
           </div>
